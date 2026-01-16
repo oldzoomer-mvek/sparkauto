3fceb2818b956ccc628b0afabfd6eda9744fb454
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2026-01-16 15:58:29 +0300

    Fix some security moments

--- src/main/java/ru/oldzoomer/security/SecurityConfig.java
+++ src/main/java/ru/oldzoomer/security/SecurityConfig.java
@@ -18,14 +18,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     @Bean
-    public UserDetailsService users() {
+    UserDetailsService users() {
         return new InMemoryUserDetailsManager(
             new User("admin", "{noop}admin",
-            List.of(new SimpleGrantedAuthority("ADMIN"))));
+            List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))));
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/frontend/**",
